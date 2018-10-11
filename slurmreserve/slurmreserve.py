f609e2b3a6f24a1b3adde51bd2e1e7dee440ad86
Johannes Reppin <johannes.reppin@desy.de>
2018-10-11 17:20:21 +0200

    added python function barebones

---
+++ slurmreserve/slurmreserve.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+import time
+import datetime
+import sys
+import pyslurm 
+
+def parse_time:
+    pass
+
+def parse_reservation:
+    pass
+
+def create_dict:
+    return res_dict
+
+def get_reservation(partition):
+    return res_dict 
+
+def create_reservation(partition, *args):
+    return res_id
+
+def get_partition(username):
+    return partition
+
+def update_reservation(res_dict):
+    return 
+
+def delete_reservation(res_id, reason):
+    return
