8f9b61bf354dfe30f14029e0cd980b414b27bf0f
Sebastian Rose <43998160+zetoss@users.noreply.github.com>
2018-10-12 12:55:31 +0200

    get_reservation, parse_time

--- slurmreserve/slurmreserve.py
+++ slurmreserve/slurmreserve.py
@@ -5,23 +5,38 @@ import datetime
 import sys
 import pyslurm 
 
-def parse_time:
+def parse_time(timestamp):
+    return datetime.datetime.fromtimestamp(timestamp).strftime('%d %B %Y %H:%M:%S')
+
+def parse_reservation():
     pass
 
-def parse_reservation:
-    pass
-
-def create_dict:
+def create_dict():
     return res_dict
 
 def get_reservation(partition):
-    return res_dict 
+	res = pyslurm.reservation()
+	res.get();
+	keys  = res.find("partition", partition)
+
+	reservations = []
+
+	for key in keys:
+		tmp = res.find_id(key)
+		tmp["start_time"] = parse_time(tmp["start_time"])
+		tmp["end_time"] = parse_time(tmp["end_time"])
+		reservations.append(tmp)
+
+	return reservations
 
 def create_reservation(partition, *args):
     return res_id
 
 def get_partition(username):
-    return partition
+	#Read users patitions from db
+	partitions = ["cms"]
+
+	return partitions
 
 def update_reservation(res_dict):
     return 
